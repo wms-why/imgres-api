a8070976a8092f723ebf51f54837fa78e9f6f48e
ymk <mkyms@outlook.com>
2025-01-06 19:48:44 +0800

    增加tls的支持

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ mod auth;
 mod core;
 mod db;
 
+use std::io::Read;
+
 use api::resize::{resize, resize_free};
 use auth::Auth;
+use poem::listener::{RustlsCertificate, RustlsConfig};
 use poem::middleware::{CatchPanic, Cors};
 use poem::EndpointExt;
 use poem::{get, handler, listener::TcpListener, post, IntoResponse, Result, Route, Server};
@@ -33,7 +36,25 @@ async fn main() -> Result<(), std::io::Error> {
         .with(Cors::new())
         .with(CatchPanic::new());
 
-    Server::new(TcpListener::bind("0.0.0.0:53768"))
-        .run(app)
-        .await
+    let listener = if std::env::var("PROFILE").is_ok_and(|v| v == "dev") {
+        TcpListener::bind("0.0.0.0:53768");
+    } else {
+        let cert = std::fs::File::open("cert.pem").expect("Failed to get cert.pem");
+        let key = std::fs::File::open("key.pem").expect("Failed to get key.pem");
+
+        let mut cert = std::io::BufReader::new(cert);
+        let mut key = std::io::BufReader::new(key);
+
+        let cert_str = "".to_string();
+        cert.read_to_string(&mut cert_str);
+
+        let key_str = "".to_string();
+        key.read_to_string(&mut key_str);
+
+        TcpListener::bind("0.0.0.0:53768").rustls(
+            RustlsConfig::new().fallback(RustlsCertificate::new().key(key_str).cert(cert_str)),
+        )
+    };
+
+    Server::new(listener).run(app).await
 }
